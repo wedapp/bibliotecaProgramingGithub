adaf9da22b43778564a4b2258412e0832c6b067f
Daniel Alves <daniel@wedapp.io>
2017-06-29 14:52:19 -0300

    se crea el archivo de if

---
+++ swift/1_if_MyPlayground2906.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var number = 12
+if number > 10{
+    print("number almacena un valor mayor a 10")
+}
