9fdc3da26db7fb40f374447de42689824b628e58
Daniel Alves <daniel@wedapp.io>
2017-06-29 14:55:24 -0300

    se crea el archivo de switch

---
+++ swift/3_switch_MyPlayground2906.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var number = 3
+switch number {
+case 0:
+    print("la variable number contiene un 0")
+case 1:
+    print("la variable number contiene un 1")
+case 2:
+    print("la variable number contiene un 2")
+case 3:
+    print("la variable number contiene un 3")
+case 4:
+    print("la variable number contiene un 4")
+default:
+    print("la variable number contiene un número mayor a 5")
+}
