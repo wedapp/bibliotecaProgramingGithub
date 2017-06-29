fbd5274f111907a57266082f525650d550ff515b
Daniel Alves <daniel@wedapp.io>
2017-06-29 14:56:07 -0300

    se crea el archivo while

---
+++ swift/5_while_MyPlayground2606.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var indice = 1
+while indice <= 5 {
+    print(indice)
+    indice = indice + 1
+}
