00d1b68be9a3bd8bf2c7b06cfb457e351958062b
Daniel Alves <daniel@wedapp.io>
2017-06-29 14:56:30 -0300

    se crea el archivo de repeat while

---
+++ swift/6_repeat-while_MyPlayground2606.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var indice = 1
+repeat{
+    print(indice)
+    indice = indice + 1
+}while indice <= 5
