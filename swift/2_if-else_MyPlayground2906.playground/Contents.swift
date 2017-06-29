69d4dbf001f81cc45f392ebfdc9ac4114c928458
Daniel Alves <daniel@wedapp.io>
2017-06-29 14:55:04 -0300

    se crea el archivo de if else

---
+++ swift/2_if-else_MyPlayground2906.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var number = 12
+if number > 10{
+    print("number almacena un valor mayor a 10")
+}else{
+    print ("number almacena un valor menor a 10")
+}
